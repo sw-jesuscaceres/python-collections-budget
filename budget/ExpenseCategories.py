4fbc2b6332271bb83566ef186aaa001d8ceb51bd
Jesus Caceres <jesus.caceres@southworks.com>
2022-10-27 12:31:38 -0300

    Categorizing expenses into separate sets

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -5,6 +5,10 @@ def main():
     expenses = Expense.Expenses()
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
+    divided_set_comp = expenses.categorize_set_comprehension()
+    if not (divided_for_loop == divided_set_comp):
+        print("Sets are NOT equal by == test")
+    
 
 
 if __name__ == "__main__":
