27b4cc299d77471a711de13b5fda4302955173d3
Jesus Caceres <jesus.caceres@southworks.com>
2022-10-27 12:37:44 -0300

    FIX THE TEST BECAUSE ITS TOO STRICT!!!!!!!!!!

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,6 +1,7 @@
 from . import Expense
 import matplotlib.pyplot as plt
 
+
 def main():
     expenses = Expense.Expenses()
     expenses.read_expenses('data/spending_data.csv')
@@ -9,10 +10,9 @@ def main():
     if not (divided_for_loop == divided_set_comp):
         print("Sets are NOT equal by == test")
     for a, b in zip(divided_for_loop, divided_set_comp):
-        if not (a.issubset(b) and b.issubset(a)):
-            print("Sets are NOT eqaual by subset test")
-
-    
+        if not (a.issubset(b) and
+                b.issubset(a)):
+            print("Sets are NOT equal by subset test")
 
 
 if __name__ == "__main__":
